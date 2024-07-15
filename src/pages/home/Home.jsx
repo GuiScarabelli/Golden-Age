4419e819eb9328fa88a91688f91607a6e8ac9bf8
GuiScarabelli <guilherme.quadros@sptech.school>
2024-07-15 15:54:23 -0300

    Creating Golden Age front-end project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <>
+      <div className="main">
+        Salve
+      </div>
+    </>
+  );
+}
+
+export default Home;
